1bfff4bdfe07679f505c960343dd7ef8f22d92c5
Habibur Rahman <126333479+habi8@users.noreply.github.com>
2023-11-09 20:26:18 +0600

    Add files via upload

--- CSE 301/MST/kruskal.cpp
+++ CSE 301/MST/kruskal.cpp
@@ -9,7 +9,17 @@ const int N=1e5+10;
 int parent[N];
 int size[N];
 int nodes,edges;
- vector<pair<int,pair<int,int>>> E;
+ vector<pair<int,pair<string,string>>> E;
+ vector<string> vertex;
+
+ int index(string str){
+    for(int i=0;i<vertex.size();i++){
+        if(str==vertex[i]){
+            return i;
+        }
+    }
+    return -1;
+ }
 
 void Make(int v){
     parent[v]=v;
@@ -40,27 +50,31 @@ void readInput(){
     cin >> nodes >> edges;
 
     for(int i=0;i<edges;i++){
-        int u,v,w;
+        int w;
+        string u,v;
         cin >> u>>v>>w;
         E.push_back({w,{u,v}});
+        if(find(vertex.begin(),vertex.end(),u)== vertex.end()) vertex.push_back(u);
+        if(find(vertex.begin(),vertex.end(),v)== vertex.end()) vertex.push_back(v);
+
         }
 }
 void kruskal(){
     sort(E.begin(),E.end());
-        for(int i=0;i<=nodes;i++){
+        for(int i=0;i<nodes;i++){
             Make(i);
         }
         int total_cost=0;
         cout <<"Selected edges are:" <<endl;
         for(auto &E : E){
             int w=E.first;
-            int u=E.second.first;
-            int v=E.second.second;
+            int u=index(E.second.first);
+            int v=index(E.second.second);
             if(Find(u) == Find(v)) continue;
             Union(u,v);
             total_cost+=w;
 
-            cout << u << " " << v<< endl;
+            cout << vertex[u] << " " << vertex[v]<< endl;
     }
     cout << "Total cost is: " << total_cost << endl;
 }
